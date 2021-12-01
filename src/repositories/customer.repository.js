6029b28cfa92bd55ecd5922342830cf18e4c33c5
mfaubert <maite.faubert@gmail.com>
2021-12-01 12:46:35 -0500

    Route pour le PUT

--- src/repositories/customer.repository.js
+++ src/repositories/customer.repository.js
@@ -12,15 +12,25 @@ class CustomerRepository {
         return Promise.all([retrieveQuery, countQuery]);
     }
 
-    transform(customer){
+    retrieveById(idCustomer) {
+        return Customer.findById(idCustomer)
+    }
+
+    update(idCustomer, customerModifs) {
+        const customerToDotNotation = objectToDotNotation(customerModifs);
+        console.log(customerToDotNotation);
+        return Customer.findByIdAndUpdate(idCustomer, customerToDotNotation, { new: true });
+    }
+
+    transform(customer) {
 
         customer.href = `/customers/${customer._id}`;
 
         customer.age = parseFloat(((dayjs() - customer.birthday) / 31536000000).toFixed(0));
-        customer.phone = "[" + customer.phone.substring(0,4) + "]" + customer.phone.substring(4,8) + "-" + customer.phone.substring(8,14) + "@" + customer.phone.substring(14,16);
+        customer.phone = "[" + customer.phone.substring(0, 4) + "]" + customer.phone.substring(4, 8) + "-" + customer.phone.substring(8, 14) + "@" + customer.phone.substring(14, 16);
         customer.lightspeed = "[" + customer.planet + "]@(" + customer.coord.lat + ";" + customer.coord.lon + ")";
-        
-        
+
+
         delete customer._id;
 
         return customer;
--- src/routes/customer.routes.js
+++ src/routes/customer.routes.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import HttpError from 'http-errors';
 import paginate from 'express-paginate';
 
 import customerRepository from '../repositories/customer.repository.js';
@@ -11,11 +10,10 @@ const router = express.Router();
 class CustomerRoutes {
     constructor() {
         router.get('/', paginate.middleware(20, 40), this.getAll);
-        router.put('/:idCustomer', customerValidator.complete(), validator, this.put);
+        router.get('/:customerId', this.getOne);
+        router.put('/:customerId', customerValidator.complete(), validator, this.put);
     }
 
-    //Lorsque le paramètre d’URL est présent n’oublier pas d’ajuster le code pour 
-    //compter le nombre de document.
     async getAll(req, res, next) {
 
         const filter = {};
@@ -24,20 +22,20 @@ class CustomerRoutes {
         }
 
         const retrieveOptions = {
-            skip:req.skip,
-            limit:req.query.limit
+            skip: req.skip,
+            limit: req.query.limit
         };
-        
+
         try {
             let [customers, documentCount] = await customerRepository.retrieveAll(retrieveOptions, filter);
 
             customers = customers.map(c => {
-                c = c.toObject({getters:false, virtuals:false});
+                c = c.toObject({ getters: false, virtuals: false });
                 c = customerRepository.transform(c);
                 return c;
             });
 
-            const pageCount = Math.ceil(documentCount/req.query.limit);
+            const pageCount = Math.ceil(documentCount / req.query.limit);
             const hasNextPage = paginate.hasNextPages(req)(pageCount);
 
             // Pour obtenir les -1, current, +1
@@ -55,39 +53,72 @@ class CustomerRoutes {
                 },
                 _links: {
                     first: `/customers?page=1&limit=${req.query.limit}`,
-                    prev:pageArray[0].url,
-                    self:pageArray[1].url,
-                    next:pageArray[2].url,
+                    prev: pageArray[0].url,
+                    self: pageArray[1].url,
+                    next: pageArray[2].url,
                     last: `/customers?page=${pageCount}&limit=${req.query.limit}`
                 },
-                data:customers
+                data: customers
             }
 
-            // Contrôller pour avoir les bonnes informations à la première et la
-            // dernière page
-            if(req.query.page === 1) {
+            if (req.query.page === 1) {
                 delete response._links.prev;
                 response._links.self = pageArray[0].url;
                 response._links.next = pageArray[1].url;
             }
 
-            if(!hasNextPage) {
+            if (!hasNextPage) {
                 response._links.prev = pageArray[1].url;
                 response._links.self = pageArray[2].url;
                 delete response._links.next;
             }
 
             res.status(200).json(response);
-        } catch(err) {
+        } catch (err) {
             return next(err);
         }
 
     }
 
-    async put(req, res, next) {
+    async getOne(req, res, next) {
+        const idCustomer = req.params.customerId;
 
+        try {
+            let customer = await customerRepository.retrieveById(idCustomer);
+
+            if (customer) {
+                res.status(200).json(customer);
+            } else {
+                //2. J'ai pas de planète
+                return next(HttpError.NotFound(`La planète ${idCustomer} n'existe pas`));
+            }
+        } catch (err) {
+            return next(err);
+        }
     }
 
+    // Put en sql est comme un delete suivi d'un insert
+    // Si la ressource n'existe pas encore, on ne fait qu'un insert (create)
+    async put(req, res, next) {
+
+        try {
+            let customer = await customerRepository.update(req.params.customerId, req.body);
+            console.log(customer);
+
+            if (!customer) {
+                return next(HttpError.NotFound(`Le client avec l'identifiant ${req.params.customerId} n'existe pas`));
+            }
+
+            if (req.query._body === 'false') {
+                res.status(200).end();
+            } else {
+                res.status(200).json(customer);
+            }
+
+        } catch (err) {
+            return next(err);
+        }
+    }
 }
 
 new CustomerRoutes();
